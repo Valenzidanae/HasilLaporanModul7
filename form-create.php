6626689622ea2f0a9242ca25203b741cea7e4dcf
Valenzidanae <46430109+Valenzidanae@users.noreply.github.com>
2019-03-13 12:21:05 +0700

    Add files via upload

---
+++ form-create.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+    <form action="create.php" method="POST">
+        <fieldset>
+        <legend>Tambah Data Dosen</legend>
+        <p>
+            <label>Nama Dosen:</label>
+            <input type="text" name="nama_dosen" placeholder="" />
+        </p>
+        <p>
+            <label>No. Telepon:</label>
+            <input type="telp" name ="telp" placeholder="" />
+        </p>
+        <p>
+            <input type="submit" name="simpan" value="Simpan" />
+        </p>
+        </fieldset>
+    <form>
+</body>
+</html>
